bad23d816614e50cde5b51f5f80e49add17daec6
Alice Andrade <alice.andrade@gmail.com>
2013-04-15 22:54:21 +0000

    editando o texto

--- blog.php
+++ blog.php
@@ -4,7 +4,7 @@
 
 <?php $resultado = pegaPosts(10); ?>
 <?php while ($linha = mysql_fetch_array($resultado)): ?>
-<h3><a href="blog_item.php?id=<?php echo $linha['id'] ?>" title="Leia mais sobre: <?php echo $titulo = htmlentities($linha['titulo'], ENT_QUOTES, 'iso8859-1') ?>"><?php echo $titulo ?></a></h3>
+<h3><a href="blog_item.php?id=<?php echo $linha['id'] ?>" title="Leia mais disto: <?php echo $titulo = htmlentities($linha['titulo'], ENT_QUOTES, 'iso8859-1') ?>"><?php echo $titulo ?></a></h3>
 <p><?php echo htmlentities ($linha['conteudo'], ENT_QUOTES, 'iso8859-1') ?></p>
 <?php endwhile ?> 
 
